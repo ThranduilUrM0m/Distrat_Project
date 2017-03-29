75603e570704defe5a35bd232a1b374a6cf3340f
Zakariae Boutaleb <zakariaeboutaleb@gmail.com>
2017-03-29 02:57:09 +0100

    mods

--- Project_Distrat/Distrat/Distrat_ma/urls.py
+++ Project_Distrat/Distrat/Distrat_ma/urls.py
@@ -6,9 +6,11 @@ app_name = 'Distrat_ma'
 urlpatterns = [
     url('^$', views.index, name='index'),
     url('^about_us/$', views.about_us, name='about_us'),
+    url('^engagements/$', views.engagements, name='engagements'),
     url('^$', views.products, name='products'),
     url('^samples/$', views.samples, name='samples'),
     url('^showrooms/$', views.showrooms, name='showrooms'),
     url('^(?P<categorie_id>[0-9]+)/categorie/$', views.categories, name='categories'),
     url('^(?P<souscategorie_id>[0-9]+)/subcategories/$', views.subcategories, name='subcategories'),
+    url(r'^avis_register$', views.avis_register, name='avis_register'),
 ]
